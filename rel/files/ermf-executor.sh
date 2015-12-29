1ad24e4aee0a22d7533a3cfce9605258cad72eb9
Drew Kerrigan <dr.kerrigan@gmail.com>
2015-12-29 15:02:53 -0500

    making ermf-scheduler more shell friendly

--- rel/files/ermf-executor.sh
+++ rel/files/ermf-executor.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-readonly EXECUTORDIR="${PWD}/riak_mesos_executor"
-readonly EXECUTOR="${EXECUTORDIR}/bin/riak_mesos_executor"
-
 main() {
     echo "Running checks for proper environment:"
     echo "Checking that riak_mesos_executor directory exists"
-    [ -d "${EXECUTORDIR}" ] || exit
+    [ -d "riak_mesos_executor" ] || exit
     echo "Checking for riak_mesos_executor executable"
-    [ -x "${EXECUTOR}" ] || exit
+    [ -x "riak_mesos_executor/bin/riak_mesos_executor" ] || exit
     echo "Checking if HOME is set..."
-    if [ -z "${HOME}" ]; then
+    if [ -z "$HOME" ]; then
         echo "Setting HOME to $PWD"...
-        export HOME="${PWD}"
+        export HOME="$PWD"
     fi
 
     echo "Starting riak_mesos_executor..."
-    "${EXECUTOR}" console -noinput
+    riak_mesos_executor/bin/riak_mesos_executor console -noinput
 }
 
 main "$@"
