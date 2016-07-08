7000f6edc79e9fdaa9d70f0fdf7729e142e23084
Nikolay Khabarov <2xl@mail.ru>
2016-07-08 20:22:06 +0300

    move to erl_mesos

--- src/riak_mesos_executor.erl
+++ src/riak_mesos_executor.erl
@@ -1,113 +1,126 @@
 -module(riak_mesos_executor).
 
--behaviour(executor).
+-behaviour(erl_mesos_executor).
 -export([
          init/1,
-         registered/4,
+         registered/3,
+         disconnected/2,
+         reregister/2,
          reregistered/2,
-         disconnected/1,
-         launchTask/2,
-         killTask/2,
-         frameworkMessage/2,
-         shutdown/1,
-         error/2
+         launch_task/3,
+         kill_task/3,
+         acknowledged/3,
+         framework_message/3,
+         error/3,
+         shutdown/2,
+         handle_info/3,
+         terminate/3
         ]).
 -export([start_link/0]).
 
--include_lib("erlang_mesos/include/mesos_pb.hrl").
+-include_lib("erl_mesos/include/executor_protobuf.hrl").
+-include_lib("erl_mesos/include/executor_info.hrl").
 
 -record(state,
         {
          riak_node,
-         exec_info,
          framework_info,
-         slave_info,
+         agent_info,
          task_status,
          rnp_state
         }).
 
 start_link() ->
-    executor:start_link(?MODULE, []).
+    erl_mesos_executor:start_link(?MODULE, ?MODULE, [], []).
 
-%% spec specifications
--spec init(Args :: any()) -> {ok, #state{}}.
-init([]) ->
-    {ok, #state{}}.
+init(_Options) ->
+    {ok, #'Call.Subscribe'{}, #state{}}.
 
--spec registered( ExecutorInfo :: #'ExecutorInfo'{},
-                      FrameworkInfo :: #'FrameworkInfo'{},
-                      SlaveInfo :: #'SlaveInfo'{},
-                      #state{})-> {ok, #state{}}.
-registered(#'ExecutorInfo'{}=ExInfo,
-           #'FrameworkInfo'{}=FwInfo,
-           #'SlaveInfo'{}=SlInfo, #state{}=State) ->
-    lager:info("~p~n~p~n~p~n", [ExInfo, FwInfo, SlInfo]),
+registered(ExecutorInfo, EventSubscribed, State) ->
+    lager:info("registered ~p~n", [ExecutorInfo]),
+    #'Event.Subscribed'{framework_info = FrameworkInfo,
+                        agent_info = AgentInfo} = EventSubscribed,
     {ok, State#state{
-           exec_info=ExInfo,
-           framework_info=FwInfo,
-           slave_info=SlInfo
-          }}.
+                     framework_info = FrameworkInfo,
+                     agent_info = AgentInfo
+                    }}.
 
--spec reregistered(SlaveInfo :: #'SlaveInfo'{}, #state{}) -> {ok, #state{}}.
-reregistered(#'SlaveInfo'{}=SlInfo, #state{}=State) ->
+reregister(_ExecutorInfo, State) ->
+    {ok, #'Call.Subscribe'{}, State}.
+
+reregistered(ExecutorInfo, State) ->
     % TODO We need to separate e.g. lager:info/2 call in registered/2 from this
     % so that we can differentiate between info logging and stuff for CLI
-    lager:info("Re-registered Executor on slave ~p~n", [#'SlaveInfo'.hostname]),
-    {ok, State#state{slave_info=SlInfo}}.
-
--spec disconnected(#state{}) -> {ok, #state{}}.
-disconnected(#state{}=State) ->
-    lager:info("Executor disconnected"),
+    lager:info("Re-registered Executor on slave ~p~n",
+               [ExecutorInfo#executor_info.agent_host]),
     {ok, State}.
 
--spec launchTask(TaskInfo :: #'TaskInfo'{}, #state{}) -> {ok, #state{}}.
-launchTask(#'TaskInfo'{}=TaskInfo, #state{}=State) ->
-    lager:debug("launchTask: ~p~n", [TaskInfo]),
-    #'TaskInfo'{ task_id=TaskId }=TaskInfo,
-    TaskStatus = #'TaskStatus'{ task_id=TaskId, state='TASK_STARTING' },
-    {ok, driver_running} = executor:sendStatusUpdate(TaskStatus),
+disconnected(ExecutorInfo, State) ->
+    lager:info("Executor disconnected ~p~n", [ExecutorInfo]),
+    {ok, State}.
+
+launch_task(ExecutorInfo, #'Event.Launch'{task = TaskInfo}, State) ->
+    #'TaskInfo'{task_id = TaskId} = TaskInfo,
+    lager:debug("Launching task: ~p~n", [TaskId]),
+    TaskStatus = #'TaskStatus'{task_id = TaskId, state = 'TASK_STARTING'},
+    ok = erl_mesos_executor:update(ExecutorInfo, TaskStatus),
     {ok, RNPSetup} = rme_rnp:setup(TaskInfo),
     case rme_rnp:start(RNPSetup) of
         {ok, RNPStarted, Bytes} ->
-            {ok, driver_running} = executor:sendStatusUpdate(TaskStatus#'TaskStatus'{state='TASK_RUNNING', data=Bytes}),
-            {ok, State#state{task_status=TaskStatus, rnp_state=RNPStarted}};
-        {error,_}=Err ->
+            ok = erl_mesos_executor:update(ExecutorInfo,
+                                           TaskStatus#'TaskStatus'{state = 'TASK_RUNNING',
+                                                                   data = Bytes}),
+            {ok, State#state{task_status = TaskStatus, rnp_state = RNPStarted}};
+        {error,_} = Err ->
             lager:warning("Failed to start: ~p", [Err]),
-            {ok, driver_running} = executor:sendStatusUpdate(TaskStatus#'TaskStatus'{state='TASK_FAILED'}),
+            ok = erl_mesos_executor:update(ExecutorInfo,
+                                           TaskStatus#'TaskStatus'{state = 'TASK_FAILED'}),
             %% TODO Surely we need to return some other state here...
-            {ok, State#state{task_status=TaskStatus}}
+            {ok, State#state{task_status = TaskStatus}}
     end.
 
--spec killTask(TaskID :: #'TaskID'{}, #state{}) -> {ok, #state{}}.
-killTask(#'TaskID'{}=TaskId, #state{task_status=TaskStatus0, rnp_state=RNPSt0}=State) ->
+kill_task(ExecutorInfo, EventKill,
+          #state{task_status = TaskStatus0, rnp_state = RNPSt0} = State) ->
+    #'Event.Kill'{task_id = TaskId} = EventKill,
     lager:debug("Killing task: ~p~n", [TaskId]),
     ok = rme_rnp:stop(RNPSt0),
-    TaskStatus = TaskStatus0#'TaskStatus'{state='TASK_KILLED'},
-    {ok, driver_running} = executor:sendStatusUpdate(TaskStatus),
-    {ok, State#state{task_status=TaskStatus}}.
+    TaskStatus = TaskStatus0#'TaskStatus'{state = 'TASK_KILLED'},
+    ok = erl_mesos_executor:update(ExecutorInfo,
+                                   TaskStatus#'TaskStatus'{state = 'TASK_KILLED'}),
+    {ok, State#state{task_status = TaskStatus}}.
 
--spec frameworkMessage(Message :: string(), #state{}) -> {ok, #state{}}.
-frameworkMessage("finish", #state{task_status=TaskStatus0, rnp_state=RNPSt0}=State) ->
-    lager:debug("Force finishing riak node"),
-    TaskStatus = TaskStatus0#'TaskStatus'{state='TASK_FINISHED'},
-    ok = rme_rnp:force_stop(RNPSt0),
-    {ok, driver_running} = executor:sendStatusUpdate(TaskStatus),
-    {ok, State#state{task_status=TaskStatus, rnp_state=undefined}};
-frameworkMessage(Message, #state{}=State) ->
-    lager:debug("Got framework message: ~s~n", [Message]),
+acknowledged(_ExecutorInfo, EventAcknowledged, State) ->
+    lager:debug("Acknowledged: ~p~n", [EventAcknowledged]),
     {ok, State}.
 
--spec shutdown(#state{}) -> {ok, #state{}}.
-shutdown(#state{task_status=TaskStatus0, rnp_state=RNPSt0}=State) ->
+framework_message(ExecutorInfo, #'Event.Message'{data = <<"finish">>},
+                  State) ->
+    lager:debug("Force finishing riak node"),
+    #state{task_status = TaskStatus0, rnp_state = RNPSt0} = State,
+    TaskStatus = TaskStatus0#'TaskStatus'{state='TASK_FINISHED'},
+    ok = rme_rnp:force_stop(RNPSt0),
+    ok = erl_mesos_executor:update(ExecutorInfo, TaskStatus),
+    {ok, State#state{task_status = TaskStatus, rnp_state = undefined}};
+framework_message(_ExecutorInfo, EventMessage, State) ->
+    lager:debug("Got framework message: ~s~n", [EventMessage]),
+    {ok, State}.
+
+shutdown(_ExecutorInfo, State) ->
+    #state{task_status = TaskStatus0, rnp_state = RNPSt0} = State,
     lager:info("Shutting down the executor"),
     TaskStatus = TaskStatus0#'TaskStatus'{state='TASK_KILLED'},
     ok = rme_rnp:stop(RNPSt0),
-    %% Remember: the behaviour takes care of stopping the process appropriately
-    %% not us.
-    {ok, driver_running} = executor:sendStatusUpdate(TaskStatus),
-    {ok, State#state{task_status=TaskStatus}}.
+    {stop, State#state{task_status=TaskStatus}}.
 
--spec error(Message :: string(), #state{}) -> {ok, #state{}}.
-error(Message, #state{}=State) ->
-    lager:debug("Got error message: ~p~n", [Message]),
+error(_ExecutorInfo, EventError, State) ->
+    lager:debug("Got error message: ~p~n", [EventError]),
     {ok, State}.
+
+handle_info(_ExecutorInfo, _Info, State) ->
+    {ok, State}.
+
+terminate(_ExecutorInfo, Reason, State) ->
+    #state{rnp_state = RNPSt0} = State,
+    lager:info("Terminating the executor, reasons: ~p~n", [Reason]),
+    ok = rme_rnp:force_stop(RNPSt0),
+    stop.
